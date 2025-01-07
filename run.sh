5e949db7b014c50fa656957e1ea923f9a7f96e0d
miiiingi <kimmingi722@gmail.com>
2025-01-07 11:16:43 +0900

    fix: 쉘스크립트의 명령행 인자를 통해서 Deep sort를 사용할 것인지 아닌지 결정하도록 수정

--- run.sh
+++ run.sh
@@ -1,1 +1,23 @@
- ./demo ../resources/deepsort.engine ../resources/demo2.mp4 ../resources/yolov8s.onnx DS
+#!/bin/bash
+
+# 명령행 인자가 1개인지 확인 (NDS만 받기 때문)
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <NDS>"
+    exit 1
+fi
+
+# 하드코딩된 경로
+engine_path="../resources/deepsort.engine"
+video_path="../resources/demo2.mp4"
+onnx_path="../resources/yolov8s.onnx"
+
+# 명령행 인자에서 NDS 값 추출
+nds=$1  # NDS 값
+
+# 추출한 값을 출력 (실제로는 이 값을 사용하여 로직을 수행)
+echo "Engine Path: $engine_path"
+echo "Video Path: $video_path"
+echo "ONNX Path: $onnx_path"
+echo "NDS: $nds"
+
+ ./demo $engine_path $video_path $onnx_path $nds
